d7f71e59209ddf88f656bc7df0f180a7b2ddeb8b
SrPabvliss <pablomartinvillacres@gmail.com>
2023-12-04 23:39:51 -0500

    Clases file modifyed, (18) Override anotation

--- 01-dart-intro/06_clases.dart
+++ 01-dart-intro/06_clases.dart
@@ -1,16 +1,21 @@
 void main() {
-  final wolverine = new Hero('Wolverine', 'Regeneración');
+  final wolverine = new Hero(nombre: 'Logan', power: 'Regeneración');
   print(wolverine.nombre);
   print(wolverine.power);
+  print(wolverine.toString());
 }
 
 class Hero {
   String nombre;
   String power;
 
-  Hero(this.nombre, this.power);
+  Hero({required this.nombre, required this.power});
 
   //Hero(String name, String power)
   //    : this.nombre = name,
-  //      this.power = power;
+  //      this.power = power;\
+
+  // @override indica que se está sobreescribiendo un método
+  @override
+  String toString() => 'nombre: $nombre - power: $power';
 }
