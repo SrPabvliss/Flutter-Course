0080cd8eee9b840bef5bd653bf90047fd72b6c7b
SrPabvliss <pablomartinvillacres@gmail.com>
2023-12-05 00:46:12 -0500

    08_setters_getters file created, (20) Setters and getters on dart

---
+++ 01-dart-intro/08_setters_getters.dart
@@ -0,0 +1,22 @@
+void main() {
+  final cuadrado = Square(side: 4);
+  print(cuadrado.area);
+}
+
+class Square {
+  double _side;
+
+  //el guion bajo siginifica que el atributo es privado
+
+  Square({required double side}) : _side = side;
+
+  set side(double value) {
+    print('cambiando el valor de size');
+    if (value < 0) throw 'Value must be greater than 0';
+    _side = value;
+  }
+
+  double get area {
+    return this._side * this._side;
+  }
+}
